10cae05f4374a14077b0daa7f1285c88110a7b65
batanoffs <batanoff.s@protonmail.com>
2024-06-19 14:29:02 +0300

    updated express config and router

--- earth-treasure-vaul/src/config/express.js
+++ earth-treasure-vaul/src/config/express.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const { secret } = require("../constants/identity");
+const { session } = require("../middlewares/session");
 
 function configExpress(app) {
-    app.use(cookieParser);
+    app.use(cookieParser({ secret }));
+    app.use(session())
     //TODO add session middleware
 
     app.use("/static", express.static("static"));
